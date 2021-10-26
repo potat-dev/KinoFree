18d9848747ecab8ebe58ecd8221edbfc8ed14a8c
Den <denis100703@gmail.com>
2021-10-27 02:27:00 +0300

    first working version!

--- popup.js
+++ popup.js
@@ -1,24 +1,44 @@
-// Initialize butotn with users's prefered color
-let changeColor = document.getElementById("changeColor");
+var btn = document.getElementById("play_btn");
 
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
-
-// When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
+btn.addEventListener("click", async () => {
+  var [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    function: setPageBackgroundColor,
+    function: main,
   });
 });
 
-// The body of this function will be execuetd as a content script inside the
-// current page
-function setPageBackgroundColor() {
-  chrome.storage.sync.get("color", ({ color }) => {
-    document.body.style.backgroundColor = color;
-  });
-}
+// - Алгоритм:
+// - ищем ссылку на фильм
+// - если нет ссылки:
+//     - нажимаем на кнопку детали
+// - снова ищем ссылку
+// - если не находим
+//     - просим пользователя выбрать фильм
+//     - пробуем снова через 5 сек
+//     - выводим уведомление если находим
+
+function main() {
+  var url2 = "";
+  var btn_text = "Детали";
+
+  if (Array.from(document.querySelectorAll('button')).find(e => e.textContent === btn_text) === undefined) {
+    alert("Выберите фильм");
+  } else {
+    Array.from(document.querySelectorAll('button')).find(e => e.textContent === btn_text).click();
+
+    setTimeout(function () {
+      var urls = [];
+      for (var i = document.links.length; i-- > 0;)
+        if (document.links[i].href.includes("www.kinopoisk.ru/film/"))
+          urls.push(document.links[i].href);
+          
+      if (urls === []) { } else {
+        var url = urls[0];
+        url2 = url.replace(/www\.kinopoisk\.ru\/(film|series)\//gi, '4h0y.gitlab.io/#');
+        console.log(url, url2);
+        window.open(url2, "_blank");
+      }
+    }, 850);
+  }
+};
