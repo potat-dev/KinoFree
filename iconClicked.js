4f6919977098851c086cedfb7c622ac6b508f100
Den <denis100703@gmail.com>
2021-10-27 04:22:00 +0300

    js code optimized

--- iconClicked.js
+++ iconClicked.js
@@ -1,23 +1,39 @@
-var url2 = "";
-var btn_text = "Детали";
+// - Алгоритм:
+// - ищем ссылку на фильм
+// - если нет ссылки:
+//     - нажимаем на кнопку детали
+// - снова ищем ссылку
+// - если не находим
+//     - просим пользователя выбрать фильм
+//     - пробуем снова через 5 сек
+//     - выводим уведомление если находим
 
-if (Array.from(document.querySelectorAll('button')).find(e => e.textContent === btn_text) === undefined) {
+if (get_button("Детали") === undefined) {
   alert("Выберите фильм");
-  window.open("popup.html", "extension_popup", "width=300,height=400,status=no,scrollbars=yes,resizable=no");
 } else {
-  Array.from(document.querySelectorAll('button')).find(e => e.textContent === btn_text).click();
+  get_button("Детали").click();
+  
+  setTimeout(function () {
+    var urls = get_urls();
+    if (urls === []) { alert("Ссылка не найдена"); } else {
+      window.open(url_replace(urls[0]), "_blank");
+    }
+  }, 850);
+}
 
-    setTimeout(function () {
-    var urls = [];
+
+function get_button(text) {
+  return Array.from(document.querySelectorAll('button')).find(e => e.textContent === text);
+}
+
+function url_replace(url) {
+  return url.replace(/www\.kinopoisk\.ru\/(film|series)\//gi, '4h0y.gitlab.io/#');
+}
+
+function get_urls() {
+  var urls = [];
     for (var i = document.links.length; i-- > 0;)
       if (document.links[i].href.includes("www.kinopoisk.ru/film/"))
         urls.push(document.links[i].href);
-        
-    if (urls === []) { } else {
-      var url = urls[0];
-      url2 = url.replace(/www\.kinopoisk\.ru\/(film|series)\//gi, '4h0y.gitlab.io/#');
-      console.log(url, url2);
-      window.open(url2, "_blank");
-    }
-  }, 850);
+  return urls
 }
