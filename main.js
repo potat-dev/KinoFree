b73c63942471fbef50206c4ec0ba30f1d4a27297
Den <denis100703@gmail.com>
2021-11-02 01:56:00 +0300

    KP and KP HD urls parser working

--- main.js
+++ main.js
@@ -16,11 +16,6 @@ if (/https:\/\/www\.kinopoisk\.ru.*/g.test(url)) { // кинопоиск
 
 // different parse methods //
 
-function kp_parse() {
-  let data = window.location.toString().match(/https:\/\/www\.kinopoisk\.ru\/(film|series)\/(\d{5,7})\/.*/);
-  alert(`type: ${data[1]}, id: ${data[2]}`);
-}
-
 function kphd_parse() {
   if (get_button("Детали") === undefined) {
     alert("Выберите фильм");
@@ -35,12 +30,20 @@ function kphd_parse() {
   }
 }
 
+function kp_parse() {
+  window.open('https://4h0y.gitlab.io/#' + get_url_id(window.location.toString()), "_blank");
+}
+
 function unknown_parse() {
-  // alert("[неизвестный сайт]: Упс! эта часть кода еще не реализована :(");
-  alert(get_urls().join('\n\n'))
+  // alert("Упс! эта часть кода еще не реализована :(");
+  alert("Все доступные ссылки:\n\n" + get_urls().join('\n\n'))
 }
 
 // useful functions //
+
+function get_url_id(url) {
+  return url.match(/https:\/\/www\.kinopoisk\.ru\/(film|series)\/(\d{5,7})\/.*/)[2];
+}
 
 function get_button(text) {
   return Array.from(document.querySelectorAll('button')).find(e => e.textContent === text);
@@ -52,14 +55,14 @@ function url_replace(url) {
 
 function get_urls() {
   var urls = [];
-    for (var i = document.links.length; i-- > 0;)
+    for (var i = document.links.length; i--> 0;)
       if (document.links[i].href.startsWith("https://www.kinopoisk.ru/film/") || document.links[i].href.startsWith("https://www.kinopoisk.ru/series/"))
         urls.push(document.links[i].href);
   return urls
 }
 
 function has_urls() {
-  for (var i = document.links.length; i-- > 0;)
+  for (var i = document.links.length; i--> 0;)
     if (document.links[i].href.startsWith("https://www.kinopoisk.ru/film/") || document.links[i].href.startsWith("https://www.kinopoisk.ru/series/")) return 1;
   return 0;
 }
