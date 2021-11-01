8fb353aef88f63ef292f4d32839d6c21a80c257f
Den <denis100703@gmail.com>
2021-11-02 01:30:51 +0300

    implemented kinopoisk url parse + unused files removed

--- main.js
+++ main.js
@@ -17,7 +17,8 @@ if (/https:\/\/www\.kinopoisk\.ru.*/g.test(url)) { // кинопоиск
 // different parse methods //
 
 function kp_parse() {
-  alert("Упс! эта часть кода еще не реализована :(");
+  let data = window.location.toString().match(/https:\/\/www\.kinopoisk\.ru\/(film|series)\/(\d{5,7})\/.*/);
+  alert(`type: ${data[1]}, id: ${data[2]}`);
 }
 
 function kphd_parse() {
@@ -35,7 +36,8 @@ function kphd_parse() {
 }
 
 function unknown_parse() {
-  alert("[неизвестный сайт]: Упс! эта часть кода еще не реализована :(");
+  // alert("[неизвестный сайт]: Упс! эта часть кода еще не реализована :(");
+  alert(get_urls().join('\n\n'))
 }
 
 // useful functions //
@@ -51,13 +53,13 @@ function url_replace(url) {
 function get_urls() {
   var urls = [];
     for (var i = document.links.length; i-- > 0;)
-      if (document.links[i].href.includes("www.kinopoisk.ru/film/"))
+      if (document.links[i].href.startsWith("https://www.kinopoisk.ru/film/") || document.links[i].href.startsWith("https://www.kinopoisk.ru/series/"))
         urls.push(document.links[i].href);
   return urls
 }
 
 function has_urls() {
   for (var i = document.links.length; i-- > 0;)
-    if (document.links[i].href.includes("www.kinopoisk.ru/film/")) return 1;
+    if (document.links[i].href.startsWith("https://www.kinopoisk.ru/film/") || document.links[i].href.startsWith("https://www.kinopoisk.ru/series/")) return 1;
   return 0;
 }
