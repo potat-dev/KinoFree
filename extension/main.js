40d47edd3bf09823f0d057e857540b011898bb33
Den <denis100703@gmail.com>
2021-11-06 22:58:17 +0300

    show film names instead of ids on unknown page

--- extension/main.js
+++ extension/main.js
@@ -28,7 +28,7 @@ function kp_parse() {
   window.open('https://4h0y.gitlab.io/#' + get_url_id(window.location.toString()), "_blank");
 }
 
-function unknown_parse() {
+async function unknown_parse() {
   let urls = get_urls();
   let ids = [];
   for (let i = 0; i < urls.length; ++i) {
@@ -37,31 +37,48 @@ function unknown_parse() {
       ids.push(id);
     }
   }
+  // нужно реверснуть массив [::-1]
 
   if (ids.length == 0) {
     alert("Упс! Наши лучшие котики-сыщики не смогли найти ни одну поддерживаемую ссылку на этом сайте :(")
   } else if (ids.length == 1) {
     window.open('https://4h0y.gitlab.io/#' + ids[0]);
   } else {
-    alert("Найденные фильмы: " + ids.join(", "));
+    let names = await get_names(ids);
+    alert("Найденные фильмы: " + names.join(", "));
   }
 }
 
 // useful functions //
 
-function get_url_id(url) {
-  return url.match(/https:\/\/www\.kinopoisk\.ru\/(film|series)\/(\d{1,9})\/.*/)[2];
+async function get_names(ids) {
+  const timer = ms => new Promise(res => setTimeout(res, ms))
+
+  var data;
+  const output = [];
+  for (var i = 0; i < ids.length; i++) {
+    await timer(50);
+    data = await get_film_data(ids[i])
+    output.push(data.nameRu);
+    // console.log(data);
+  }
+  return output;
 }
 
-function get_button(text) {
-  return Array.from(document.querySelectorAll('button')).find(e => e.textContent === text);
+async function get_film_data(id) { //- получаем информацию о фильме -//
+  var output;
+  await fetch('https://kinopoiskapiunofficial.tech/api/v2.2/films/' + id, {
+    method: 'GET',
+    headers: {
+      'X-API-KEY': '72511645-7690-43f7-a959-0308d26defa5',
+      'Content-Type': 'application/json',
+    },
+  }).then(res => res.json())
+  .then(json => output = json).catch(err => console.log(err));
+  return output;
 }
 
-function url_replace(url) {
-  return url.replace(/www\.kinopoisk\.ru\/(film|series)\//gi, '4h0y.gitlab.io/#');
-}
-
-function get_urls() {
+function get_urls() { //- получаем все ссылки на странице -//
   var urls = [];
     for (var i = document.links.length; i--> 0;)
       if (document.links[i].href.startsWith("https://www.kinopoisk.ru/film/") || document.links[i].href.startsWith("https://www.kinopoisk.ru/series/"))
@@ -73,4 +90,16 @@ function has_urls() {
   for (var i = document.links.length; i--> 0;)
     if (document.links[i].href.startsWith("https://www.kinopoisk.ru/film/") || document.links[i].href.startsWith("https://www.kinopoisk.ru/series/")) return 1;
   return 0;
+}
+
+function get_url_id(url) {
+  return url.match(/https:\/\/www\.kinopoisk\.ru\/(film|series)\/(\d{1,9})\/.*/)[2];
+}
+
+function get_button(text) {
+  return Array.from(document.querySelectorAll('button')).find(e => e.textContent === text);
+}
+
+function url_replace(url) {
+  return url.replace(/www\.kinopoisk\.ru\/(film|series)\//gi, '4h0y.gitlab.io/#');
 }
